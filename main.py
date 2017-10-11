ed62a1e0d7eee5e1915e2ffa6dd8dc2a950a4f10
norrellborealis <tkkrings425@gmail.com>
2017-10-10 23:52:45 -0500

    Finished Maybe

--- main.py
+++ main.py
@@ -14,41 +14,57 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(220))
 
-    def __init__(self, title, textbody):
+    def __init__(self, title, body):
         self.title = title
         self.body = body
 
-@app.route('/', methods=['POST', 'GET'])
+blogs = Blog.query.all()
+
+def is_empty(n):
+    if n == "":
+        return True
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        title = request.form['title']
-        blog = request.form['blog']
-        if not title or not blog:
-            flash('Invalid title or entry', 'error')
-            return render_template('new_post.html')
+    return redirect('blog')
 
-        else:
-            new_entry = Blog(title,blog)
-            db.session.add(new_entry)
-            db.session.commit()
-            return render_template('view_entry.html', title=title, body=blog)
-    
-    blogs = Blog.query.all()
-
-    return render_template('blog_home.html', blogs=blogs)
-
-@app.route('/blog')
+@app.route('/blog', methods = ['POST', 'GET'])
 def blog():
-    id=request.args.get('id')
-    blog = Blog.query.get(id)
-    title = blog.title
-    body = blog.body
+    blog_id=request.args.get('id')
+    blog = Blog.query.filter_by(id = blog_id).first()
 
-    return render_template('view_entry.html', title=title, body=body)
+    if request.args:
+        return render_template('view_entry.html', title=blog.title, body=blog.body)
 
-@app.route('/new_entry', methods=['GET', 'POST'])
+    blogs = Blog.query.all()
+    return render_template('blog_home.html', title="New Entry", blogs=blogs)
+
+@app.route('/new_entry')
 def new_entry():
     return render_template('new_entry.html')
+
+@app.route('/new_entry', methods = ['POST'])
+def new_post():
+    title = request.form['title']
+    title_error = ''
+
+    body = request.form['body']
+    body_error = ''
+
+    if is_empty(title):
+        title_error = 'You must choose a title'
+
+    if is_empty(body):
+        body_error = 'Your post needs some content!'
+
+        return render_template('new_entry.html', title = "New Entry", title_error=title_error, body_error=body_error)
+
+    else:
+        new_blog = Blog(title, body)
+        db.session.add(new_blog)
+        db.session.commit()
+
+    return redirect('/blog?id='+str(new_blog.id))
 
 
 if __name__ == "__main__":
